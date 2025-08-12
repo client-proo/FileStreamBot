0e15518b8fdd1a28cbd49e61e83c2ebf84f5f006
client-proo <client.proo2024@gmail.com>
2025-08-12 15:03:45 +0330

    Update stream.py

--- FileStream/bot/plugins/stream.py
+++ FileStream/bot/plugins/stream.py
@@ -79,7 +79,7 @@ async def channel_receive_handler(bot: Client, message: Message):
             chat_id=message.chat.id,
             message_id=message.id,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ 📥",
+                [[InlineKeyboardButton("📥 دانلود",
                                        url=f"https://t.me/{FileStream.username}?start=stream_{str(inserted_id)}")]])
         )
 
