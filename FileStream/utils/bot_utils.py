4f613ae03a906e40c2fcfd96f414ee8570353fcf
client-proo <client.proo2024@gmail.com>
2025-11-09 00:19:53 +0330

    Update bot_utils.py

--- FileStream/utils/bot_utils.py
+++ FileStream/utils/bot_utils.py
@@ -97,8 +97,8 @@ async def gen_link(_id):
     expire_time = file_info.get('expire_at')
 
     if expires_in:
-        jalali_date = # تهران = UTC + 3:30
-tehran_time = expire_time + 12600  # 3.5 ساعت = 12600 ثانیه
+        # تهران = UTC + 3:30
+tehran_time = expire_time + 12600
 jalali_date = jdatetime.datetime.fromtimestamp(tehran_time).strftime('%Y/%m/%d - %H:%M:%S')
         remaining = int(expire_time - time.time())
         mins, secs = divmod(remaining, 60)
