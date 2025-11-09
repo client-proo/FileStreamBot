1346058b2d7c7eef8d8e218a75001796d1944b17
client-proo <client.proo2024@gmail.com>
2025-11-09 12:54:55 +0330

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -10,7 +10,6 @@ class Telegram:
     OWNER_ID = int(env.get('OWNER_ID', '7978482443'))
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     DATABASE_URL = str(env.get('DATABASE_URL'))
-    SPAM_DELAY = int(env.get("SPAM_DELAY", ""))
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
     FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
