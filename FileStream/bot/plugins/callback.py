b4e66e2056c68ff2b3a58c168fed74a23aaa88c7
client-proo <client.proo2024@gmail.com>
2025-11-10 19:31:28 +0330

    Update callback.py

--- FileStream/bot/plugins/callback.py
+++ FileStream/bot/plugins/callback.py
@@ -22,9 +22,9 @@ from FileStream.bot.plugins.admin import is_bot_active
 #---------------------[ START CMD ]---------------------#
 @FileStream.on_callback_query()
 async def cb_data(bot, update: CallbackQuery):
-    # اگر کاربر ادمین نیست، اجازه دسترسی نده
-    if update.from_user.id != Telegram.OWNER_ID:
-        await update.answer("❌ دسترسی denied. این ربات در حال حاضر فقط برای ادمین قابل استفاده است.", show_alert=True)
+    # اگر ربات خاموش است و کاربر عادی است
+    if not is_bot_active() and update.from_user.id != Telegram.OWNER_ID:
+        await update.answer("❌ ربات در حال حاضر غیرفعال است.", show_alert=True)
         return
     
     usr_cmd = update.data.split("_")
