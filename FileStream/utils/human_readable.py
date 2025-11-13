5e1fd5221a0b805b99b8b7fef7ecdda45c8f7e04
client-proo <client.proo2024@gmail.com>
2025-11-14 02:39:53 +0330

    Update human_readable.py

--- FileStream/utils/human_readable.py
+++ FileStream/utils/human_readable.py
@@ -3,8 +3,8 @@ def humanbytes(size):
         return ""
     power = 2**10
     n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    Dic_powerN = {0: ' ', 1: 'کیلو', 2: 'مگا', 3: 'گیگا', 4: 'ترا'}
     while size > power:
         size /= power
         n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'بایت'
