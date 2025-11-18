c29f390f3530b157f8e0bcee6c4149060c16062d
client-proo <client.proo2024@gmail.com>
2025-11-18 16:14:22 +0330

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -8,7 +8,7 @@ class Telegram:
     API_HASH = str(env.get("API_HASH"))
     BOT_TOKEN = str(env.get("BOT_TOKEN"))
     OWNER_ID = int(env.get('OWNER_ID', '7978482443'))
-    WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
+    WORKERS = int(env.get("WORKERS", "6"))
     DATABASE_URL = str(env.get('DATABASE_URL'))
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
@@ -20,15 +20,19 @@ class Telegram:
     START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", None))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", None))
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
-    EXPIRE_TIME = int(env.get("EXPIRE_TIME", 3600))  # پیش‌فرض 1 ساعت (به ثانیه)
-    ANTI_SPAM_TIME = int(env.get("ANTI_SPAM_TIME", 60))  # پیش‌فرض 1 دقیقه (به ثانیه)
+    EXPIRE_TIME = int(env.get("EXPIRE_TIME", 3600))
+    ANTI_SPAM_TIME = int(env.get("ANTI_SPAM_TIME", 60))
+    # اضافه کردن تنظیم جدید برای حالت فقط پرمیوم
     ONLY_PREMIUM = env.get('ONLY_PREMIUM', False)
     ONLY_PREMIUM = True if str(ONLY_PREMIUM).lower() == "true" else False
+    # اضافه کردن تنظیمات محدودیت حجمی
+    FREE_USER_MAX_SIZE = int(env.get("FREE_USER_MAX_SIZE", 0))  # 0 = نامحدود
+    PREMIUM_USER_MAX_SIZE = int(env.get("PREMIUM_USER_MAX_SIZE", 0))  # 0 = نامحدود
 
 class Server:
     PORT = int(env.get("PORT", 80))
